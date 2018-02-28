e3468ffef684da956ccded04f906fe6413862681
damoster <damoster576@gmail.com>
2018-02-28 18:12:51 +1100

    First commit

---
+++ tradeTrack.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+__author__ = "David Mo"
+__email__ = "davidmo3576@gmail.com"
+__status__ = "Production"
+
+# Python notes
+# Author
+# https://developers.google.com/gmail/api/quickstart/python
+
+# Insert class heres
+
+if __name__ == "__main__":
+	print("test")
