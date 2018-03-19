2e21bbbec72b6a6e413b995d3009bbc83071736b
damoster <damoster576@gmail.com>
2018-03-19 16:11:30 +1100

    Added load trade history from cache feature

--- tradeTrack.py
+++ tradeTrack.py
@@ -18,6 +18,7 @@ from apiclient import errors
 import email
 import base64
 import re
+import collections
 
 import tradeTracker # Separate module file with main tradeTracker module
 
@@ -136,22 +137,26 @@ def getTextBodyFromMimeMsg(mime_msg):
     elif messageMainType == 'text':
             return mime_msg.get_payload()
 
-def processMimeMsgforTradeTrack(tradeTrack, mime_msg):
-    date = mime_msg['Date']
+def processMimeMsgforTradeTrack(localTradeLogCache, mime_msg):
+    dateTime = mime_msg['Date']
     subject = mime_msg['Subject']
     txtBody = base64.urlsafe_b64decode(getTextBodyFromMimeMsg(mime_msg)).decode("utf-8")
-    # print("{}\n{}\n{}".format(date, subject, txtBody))
+    # print("{}\n{}\n{}".format(dateTime, subject, txtBody))
     
     # Skip email if it is not a trade confirmation email with the following format subject
     # "CommSec - BUB Equity Trade Confirmation"
-    # TODO update this later to handle other securities
+    # TODO update this later to handle other securities with different length ASX codes
     if not re.match(r'^CommSec - [A-Z]{3} Equity Trade Confirmation', subject):
         return
     
     # Get all the relevant information and store in the tradeTrack
-    stockCode = re.sub(r'^CommSec - ([A-Z]{3}) Equity Trade Confirmation', r'\1', subject)
+    stockCode = re.sub(r'^CommSec - ([A-Z]{3}) Equity Trade Confirmation.*', r'\1', subject)
 
-    # Just for debugging
+    # TODO Check if already in cache (User doesn't need to re-enter manual ones if already in cache) or is new email 
+    if localTradeLogCache.inCache(dateTime, stockCode):
+        return
+
+    # Just for debugging (Edge cases)
     # txtBody = 'Attached is an electronic confirmation confirming that we have SOLD for you 5000 units in CRESO PHARMA LTD FPO today on Account 2677206'
     # txtBody = 'Attached is an electronic confirmation confirming that we have BOUGHT for you 14700 units in ESENSE-LAB LTD CDI 1:1 at 0.340 on Account 2677206.'
 
@@ -161,7 +166,7 @@ def processMimeMsgforTradeTrack(tradeTrack, mime_msg):
     matchString2 = r'Attached is an electronic confirmation confirming that we have (SOLD|BOUGHT) for you ([0-9]+) units in ([^a-z]+) [a-z]+'
     if not re.search(matchString1, txtBody):
         print("! - Email body missing 'price' value, check email's pdf and enter 'stock price' specified")
-        print("     Details of email with missing info in body:\n     - Date: {}\n     - Subject: {}\n".format(date, subject))
+        print("     Details of email with missing info in body:\n     - dateTime: {}\n     - Subject: {}\n".format(dateTime, subject))
         if re.search(matchString2, txtBody):
             m = re.search(matchString2, txtBody)
             tradeType = m.group(1)
@@ -184,7 +189,7 @@ def processMimeMsgforTradeTrack(tradeTrack, mime_msg):
 
         else:
             print("another unknown email format")
-            print("Date: {}, Subject: {}".format(date, subject))
+            print("dateTime: {}, Subject: {}".format(dateTime, subject))
             exit()
     else:
         m = re.search(matchString1, txtBody)
@@ -193,39 +198,176 @@ def processMimeMsgforTradeTrack(tradeTrack, mime_msg):
         companyName = m.group(3)
         price = m.group(4)
 
-    # Store it in tradeTrack's tradeLogs
-    tradeTrack.addToTradeLog(date, stockCode, companyName, tradeType, int(units), float(price))
+    # Check if need to store in cache (Data to be stored in a file)
+    localTradeLogCache.checkAddToCache(dateTime, stockCode, companyName, tradeType, units, price)
 
-    print("{}, {}, {}, {}, {}, {}".format(date, stockCode, companyName, tradeType, units, price))
+    print("{}, {}, {}, {}, {}, {}".format(dateTime, stockCode, companyName, tradeType, units, price))
+
+def updateLogsFromGmail(localTradeLogCache):
+    # Search for messages matching query which CommSec trade confirmation emails contain
+    # Get credentials
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+    user_id = 'me' # For current authorised user
+    messages = ListMessagesMatchingQuery(service, user_id, 'subject:equity trade confirmation')
+
+    print("\nProcessing emails to update logs:\n")
+    print("---- Num Emails matching trade confiration query: "+str(len(messages)))
+    print("---- Processing email trade receipts")
+    i = 0 # TODO just for testing
+    for message in messages:
+        msg_id = message['id']
+        mime_msg = GetMimeMessage(service, user_id, msg_id)
+        processMimeMsgforTradeTrack(localTradeLogCache, mime_msg)
+        
+        i += 1
+        if i == 2:
+            break # TODO just for now
+
+class tradeLogCache():
+    def __init__(self, cachFilename):
+        self.cachFilename = 'localTradeLogCache.txt'
+        self.numDataFields = 6 # dateTime, stockCode, companyName, tradeType, units, price
+        self.logCache = {}
+
+    # Suggested Google function description by Yingran
+    # Function description
+    #
+    # Args:
+    #   someInputNme: description of the input
+    #
+    # Return:
+    #   returnName: description of the returned variable 
+
+    def writeToFile(self):
+        # Create a file
+        with open(self.cachFilename, 'w') as f:
+            od = collections.OrderedDict(sorted(self.logCache.items())) 
+            for key, v in od.items(): # Save in sorted order by key, oldest to most recent
+                tradeEntry = key+','+','.join(str(x) for x in self.logCache[key])
+                f.write(tradeEntry+'\n')
+        f.close()
+
+    # Tries to load file from local cache
+    # Returns boolean True = success or False = unsuccessful 
+    def tryLoadFromFile(self):
+        try:
+            lines = [line.rstrip('\n') for line in open(self.cachFilename)]
+            for line in lines:
+                fields = line.split(',') 
+                # Check if file contains correct number of fields
+                if len(fields) != (self.numDataFields + 1): # + 1 is for the key
+                    self.logCache = {} # Empty cache
+                    return False
+                logKey = fields[0]
+                self.logCache[logKey] = fields[1:]
+
+        except Exception as e:
+            self.logCache = {}
+            return False
+
+        # Only gets to this return statement if no errors encountered during full file load
+        return True
+
+    def getMonthNum(self, month):
+        if month == "Jan":
+            return "01"
+        elif month == "Feb":
+            return "02"
+        elif month == "Mar":
+            return "03"
+        elif month == "Apr":
+            return "04"
+        elif month == "May":
+            return "05"
+        elif month == "Jun":
+            return "06"
+        elif month == "Jul":
+            return "07"
+        elif month == "Aug":
+            return "08"
+        elif month == "Sep":
+            return "09"
+        elif month == "Oct":
+            return "10"
+        elif month == "Nov":
+            return "11"
+        elif month == "Dec":
+            return "12"
+        else:
+            return "ERROR!"
+
+    def makeLogKey(self, dateTime, stockCode):
+        t = dateTime.split(" ")
+        dateTimeKey = "{}/{}/{} {} {}".format(t[2], self.getMonthNum(t[1]), t[0], t[3], t[4]) # Special format for sorting
+        return (dateTimeKey+" - "+stockCode)
+
+    def inCache(self, dateTime, stockCode):
+        logKey = self.makeLogKey(dateTime, stockCode)
+        return (logKey in self.logCache)
+
+    # NOTE: If you change parameters of this function, need to change the global class variable: self.numDataFields
+    def checkAddToCache(self, dateTime, stockCode, companyName, tradeType, units, price):
+        if not self.inCache(dateTime, stockCode):
+            logKey = self.makeLogKey(dateTime, stockCode)
+            self.logCache[logKey] = [dateTime, stockCode, companyName, tradeType, units, price]
+
+    # Loads logs from cache into the tradTracker object 
+    # TODO maybe should just combine this class with tradeTrack object later
+    def loadIntoTradeTrack(self, tradeTrack):
+        # Store it in tradeTrack's tradeLogs (Data for Dynamic memory)
+        od = collections.OrderedDict(sorted(self.logCache.items())) 
+        for key, v in od.items(): # Save in sorted order by key, oldest to most recent
+            dateTime = v[0]
+            stockCode = v[1]
+            companyName = v[2]
+            tradeType = v[3]
+            units = v[4]
+            price = v[5]
+            tradeTrack.addToTradeLog(dateTime, stockCode, companyName, tradeType, units, price)
 
 def main():
+    # TODO update description
     """Shows basic usage of the Gmail API.
 
     Creates a Gmail API service object and outputs a list of label names
     of the user's Gmail account.
     """
 
-    # Check if there are cached trade logs
+    # TODO Check if there are cached trade logs
 
-    # Get credentials
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    service = discovery.build('gmail', 'v1', http=http)
+    # Plan for cache:
+    # 1) Check if cache exists, if so load, then go to step 2, else load all from email then skip step 2
+    #       Cache format to just be what's saved from MiMe as a txt/csv like format
+    #       When loaded, cache to be a dict with key = 'dateTime - stockCode'
+    # 2) Ask if want to update from emails, make it so that manual input values loaded from cache
+    # 3) To hard restart, need to delete cache yourself
 
-    # Search for messages matching query which CommSec trade confirmation emails contain
+    # Main objects in use
     tradeTrack = tradeTracker.tradeTracker() # Module for tracking trades
-    user_id = 'me' # For current authorised user
-    messages = ListMessagesMatchingQuery(service, user_id, 'subject:equity trade confirmation')
+    cachFilename='localTradeLogCache.txt'
+    localTradeLogCache = tradeLogCache(cachFilename) # Once separate moduel: tradeLogCache.tradeLogCache()
 
-    print("Num Emails matching trade confiration query: "+str(len(messages)))
-    print("Processing email trade receipts")
-    for message in messages:
-        msg_id = message['id']
-        mime_msg = GetMimeMessage(service, user_id, msg_id)
-        processMimeMsgforTradeTrack(tradeTrack, mime_msg)
-        
-        exit() # TODO just for now
+    # Check if can load file from cache
+    print("TradeTrack (TM) DavidMo")
+    print("---- To hard reset Cache, need to delete the file: [{}]".format(cachFilename))
+    if localTradeLogCache.tryLoadFromFile():
+        print("---- Trade history successfly loaded from trade log cache ----")
+        confirm = ""
+        while not (confirm == "y" or confirm == "n"):
+            confirm = input("     Update cache from emails? [y/n]: ")
+            if confirm == "y":
+                updateLogsFromGmail(localTradeLogCache)
+            elif confirm == "n":
+                break
+            else:
+                continue
+    else:
+        updateLogsFromGmail(localTradeLogCache)
 
+    localTradeLogCache.writeToFile()
+    localTradeLogCache.loadIntoTradeTrack(tradeTrack) # Load data from cache into tradeTrack
 
     # Capital gains/losses info
     # http://www.thebull.com.au/experts/a/277-how-do-you-calculate-capital-gains-and-losses-on-share-trades.html
--- tradeTracker.py
+++ tradeTracker.py
@@ -1,82 +1,80 @@
 
 # trade object storing transaction information
 class tradeRecord():
-	def __init__(self, date, stockCode, companyName, tradeType, units, price, brokerageFee):
-		self.date = date
-		self.stockCode = stockCode
-		self.companyName = companyName
-		self.tradeType = tradeType
-		self.units = units
-		self.price = price # in AUD
-		self.brokerageFee = brokerageFee # TODO need to handle 'International Shares'
+    def __init__(self, dateTime, stockCode, companyName, tradeType, units, price, brokerageFee):
+        self.dateTime = dateTime
+        self.stockCode = stockCode
+        self.companyName = companyName
+        self.tradeType = tradeType
+        self.units = int(units)
+        self.price = float(price) # in AUD
+        self.brokerageFee = brokerageFee # TODO need to handle 'International Shares'
 
 # stockTrade object used in logs for a specific stock
 class stockTrade():
-	def __init__(self, date, price, tradeType, units, brokerageFee):
-		self.date = date
-		self.price = price
-		self.units = units
-		self.brokerageFee = brokerageFee
+    def __init__(self, dateTime, price, tradeType, units, brokerageFee):
+        self.dateTime = dateTime
+        self.price = price
+        self.units = units
+        self.brokerageFee = brokerageFee
 
 # heldStock object storing current status of a stock held in portfolio
 class heldStock():
-	def __init__(self, companyName, unitsHeld=0, aveSharePrice=0):
-		self.companyName = companyName
-		self.unitsHeld = unitsHeld
-		self.aveSharePrice = aveSharePrice
-		self.stockTradeLog = []
+    def __init__(self, companyName, unitsHeld=0, aveSharePrice=0):
+        self.companyName = companyName
+        self.unitsHeld = unitsHeld
+        self.aveSharePrice = aveSharePrice
+        self.stockTradeLog = []
 
 # object used in tradeSummary for recording sell transaction
 class sellTransaction():
-	def __init__(self, date, stockCode, units, aveBuySharePrice, sellPrice, brokerageFee):
-		self.date = date
-		self.stockCode = stockCode
-		self.units = units
-		self.aveBuySharePrice = aveBuySharePrice
-		self.sellPrice = sellPrice
-		self.PL = (sellPrice - aveBuySharePrice) * units
-		self.brokerageFee = brokerageFee
+    def __init__(self, dateTime, stockCode, units, aveBuySharePrice, sellPrice, brokerageFee):
+        self.dateTime = dateTime
+        self.stockCode = stockCode
+        self.units = units
+        self.aveBuySharePrice = aveBuySharePrice
+        self.sellPrice = sellPrice
+        self.PL = (sellPrice - aveBuySharePrice) * units
+        self.brokerageFee = brokerageFee
 
 # tradeSummary object storing transactions of sold stocks and trading summary
 # NOTE: PL stands for profit/loss
 class tradeSummary():
-	def __init__(self):
-		self.grossTotalRealisedPL = 0 # before taking into account brokerage fees
-		self.grossTotalPaperPL = 0 # TODO later using stock price API from somewhere
-		self.totalBrokerageFees = 0
-		self.sellTransactions = [] # List of sellTransaction objects
+    def __init__(self):
+        self.grossTotalRealisedPL = 0 # before taking into account brokerage fees
+        self.grossTotalPaperPL = 0 # TODO later using stock price API from somewhere
+        self.totalBrokerageFees = 0
+        self.sellTransactions = [] # List of sellTransaction objects
 
 # Main module handling all the trade information
 class tradeTracker():
     def __init__(self):
-    	self.tradeLog = [] # List of trade objects
-    	self.portfolio = {} # Dict of heldStock objects with stockCode as dict key
-    	self.summary = tradeSummary()
+        self.tradeLog = [] # List of trade objects
+        self.portfolio = {} # Dict of heldStock objects with stockCode as dict key
+        self.summary = tradeSummary()
 
-    def calculateBrokerageFee(self, transactionValue):
+    def calculateBrokerageFee(self, units, price):
+        # NOTE: current brokerage values based on trade excecutions on the following type
+        # 'Trade online and settle your trade to a CDIA or CommSec Margin Loan'
+        transactionValue = int(units) * float(price)
+        if transactionValue <= 1000:
+            brokerageFee = 10
+        elif (transactionValue > 1000) and (transactionValue <= 10000):
+            brokerageFee = 19.95
+        elif (transactionValue > 10000) and (transactionValue <= 25000):
+            brokerageFee = 29.95
+        else:
+            brokerageFee = 0.0012 * transactionValue
 
-    	if transactionValue <= 1000:
-    		brokerageFee = 10
-    	elif (transactionValue > 1000) and (transactionValue <= 10000):
-    		brokerageFee = 19.95
-    	elif (transactionValue > 10000) and (transactionValue <= 25000):
-    		brokerageFee = 29.95
-    	else:
-    		brokerageFee = 0.0012 * transactionValue
+        return brokerageFee
 
-    	return brokerageFee
-
-    def addToTradeLog(self, date, stockCode, companyName, tradeType, units, price):
-    	brokerageFee = self.calculateBrokerageFee(units * price)
-    	self.tradeLog.append(tradeRecord(date, stockCode, companyName, tradeType, units, price, brokerageFee))
+    def addToTradeLog(self, dateTime, stockCode, companyName, tradeType, units, price):
+        brokerageFee = self.calculateBrokerageFee(units, price)
+        self.tradeLog.append(tradeRecord(dateTime, stockCode, companyName, tradeType, units, price, brokerageFee))
 
     def updatePortfolio(self):
-    	# Go through trade log from oldest to newest
-    	for i in reversed(range(len(tradeLog)-1)):
-    		trade = tradeLog[i]
-
-    # information I need
-    # - Subject for getting the 'stock code'
-    # - Message body for getting the 'units', 'BOUGHT/SOLD', 'price'
-    # - Make a function to calc brokerage fee 
+        # Go through trade log from oldest to newest
+        for i in reversed(range(len(tradeLog)-1)):
+            trade = tradeLog[i]
+            # TODO 
 
